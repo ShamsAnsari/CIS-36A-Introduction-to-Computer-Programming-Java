7d8bd7b4e9b6d06c65e690d4d9a7e352d084ee41
ShamsAnsari <36138161+ShamsAnsari@users.noreply.github.com>
2018-07-06 15:16:41 -0700

    Added Time wait

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 public class HelloWorld
 {
 	public static void main(String[] args)
@@ -6,7 +7,21 @@ public class HelloWorld
 		System.out.print("What is your name? ");
 		Scanner user = new Scanner(System.in);
 		String name = user.next();
+		
 		System.out.println("Hello," + name);
-		System.out.println("How are you " + name + "?");
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("How are you, " + name + "?");
+		String response1 = user.next();
+		try {
+			TimeUnit.SECONDS.sleep(3);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		System.out.println("Shut up, " + name + "!");
+		
 	}
 }
